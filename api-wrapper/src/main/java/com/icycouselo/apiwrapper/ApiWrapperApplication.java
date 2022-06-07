cc3b63eac50e893db5eef0e29c87fdbf1928f341
Icy Couselo <s.hellings.salt@gmail.com>
2022-06-07 21:09:39 +0200

    Initial commit

---
+++ api-wrapper/src/main/java/com/icycouselo/apiwrapper/ApiWrapperApplication.java
@@ -0,0 +1,11 @@
+package com.icycouselo.apiwrapper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiWrapperApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ApiWrapperApplication.class, args);
+    }
+}
---
+++ api-wrapper/src/main/java/com/icycouselo/apiwrapper/controller/RFNController.java
@@ -0,0 +1,7 @@
+package com.icycouselo.apiwrapper.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class RFNController {
+}
